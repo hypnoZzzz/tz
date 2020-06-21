32b256d82e74fa7d3b5e7939e50943a3401eca15
hypnoZzzz <phin_crow@inbox.ru>
2020-06-21 22:10:58 +0300

    three

--- views.py
+++ views.py
@@ -1,8 +1,12 @@
 import glob
 import os
-from PIL import Image
+
 from flask import Flask, request, redirect, url_for, send_from_directory
+from PIL import Image
 from werkzeug.utils import secure_filename
+
+magic_numbers_png = {'png': bytes([0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A])}
+magic_numbers_jpg = {'jpg': bytes([0xFF, 0xD8, 0xFF])}
 
 UPLOAD_FOLDER = 'uploads'
 ALLOWED_EXTENSIONS = set(['png', 'jpg'])
@@ -18,52 +22,24 @@ def allowed_file(filename):
            filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
 
-def format_check():
-    """проверяет соответствие формата файла указанному"""
-
-    magic_numbers_png = {'png': bytes([0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A])}
-    magic_numbers_jpg = {'jpg': bytes([0xFF, 0xD8, 0xFF])}
-    max_read_size_png = max(len(m) for m in magic_numbers_png.values())
-    max_read_size_jpg = max(len(m) for m in magic_numbers_jpg.values())
-    format = []
-
-    os.chdir('uploads')
-
-    def png():
-        for x in glob.glob("*png"):
-            with open(x, 'rb') as fd:
-                file_head = fd.read()
-
-            if file_head.startswith(magic_numbers_png['png']):
-                format.append('png')
-            else:
-                format.append("Error 302")
-
-    def jpg():
-        for y in glob.glob("*jpg"):
-            with open(y, 'rb') as fd:
-                file_head = fd.read()
-
-            if file_head.startswith(magic_numbers_jpg['jpg']):
-                format.append('jpg')
-            else:
-                format.append("Error 302")
-    return format
-
-
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
-    """загружает файл и перенаправляет пользователя на URL с загруженным файлом"""
+    """загружает файл, проверяет действителен ли формат
+     и перенаправляет пользователя на URL с загруженным файлом"""
     if request.method == 'POST':
         file = request.files['file']
-        if file and allowed_file(file.filename) and format != "Error 302":
+        if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            print(str(format))
-            return redirect(url_for('uploaded_file',
-                                    filename=filename))
-        else:
-            return "Error 302. Invalid file format. Add image format 'jpg' or 'png'"
+            os.chdir('uploads')
+            with open(file.filename, 'rb') as fd:
+                file_head = fd.read()
+            if file_head.startswith(magic_numbers_jpg['jpg']) or file_head.startswith(magic_numbers_png['png']):
+                return redirect(url_for('uploaded_file',
+                                        filename=filename))
+            else:
+                os.remove(file.filename)
+                return "Error 302. Invalid file format. Add image format 'jpg' or 'png'. File deleted"
     return '''
     <!doctype html>
     <title>Upload new File</title>
