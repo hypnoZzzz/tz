eb1c79b850f99ef4e7f38658a0176535b6fd25a1
hypnoZzzz <phin_crow@inbox.ru>
2020-06-24 17:50:48 +0300

    добавил БД

--- example.py
+++ example.py
@@ -1,8 +1,11 @@
 # не пойму, как сделать проверку, что пользователь не отправил пустую строку
-# позже функции проверки и сохранения вынесу в отдельный модуль
+import json
 import os
+import io
 
+from datetime import datetime
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 from PIL import Image
 from werkzeug.utils import secure_filename
 
@@ -11,9 +14,35 @@ magic_numbers_jpg = {'jpg': bytes([0xFF, 0xD8, 0xFF])}
 
 UPLOAD_FOLDER = 'uploads'
 ALLOWED_EXTENSIONS = set(['png', 'jpg'])
+MAX_CONTENT_LENGTH = 16*1024*1024  # 16 МБ
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tasks.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+db = SQLAlchemy(app)
+
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(150), nullable=False)
+    height = db.Column(db.Integer, default=1)
+    width = db.Column(db.Integer, default=1)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+    img_id = db.Column(db.Integer, db.ForeignKey('img.id'))
+
+    def __repr__(self):
+        return '<Task %r>' % self.id
+
+
+class Img(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String())
+    path = db.Column(db.Text)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<image id={}, name={}, path={}>'.format(self.id, self.name, self.path)
 
 
 def check_extension(filename):
@@ -43,10 +72,10 @@ def check_integrity():
         img = Image.open('uploads/' + file.filename)
         try:
             img.verify()
-            print('Valid image')  # пока оставлю принт, чтобы видеть, что работает
+            # print('Valid image')
             return True
         except Exception:
-            print('Invalid image')  # пока оставлю принт, чтобы видеть, что работает
+            # print('Invalid image')
             return False
 
 
@@ -67,8 +96,26 @@ def upload_file():
         if check_format() is not True or check_integrity() is not True:
             return render_template('error.html'), os.remove('uploads/' + file.filename)
         else:
-            return render_template('uploads.html')
+            return render_template('uploads.html'), save_to_database(filename=file.filename)
     return render_template('index.html')
+
+
+def save_to_database(filename):
+    """Получает имя файла, путь к файлу
+    и сохраняет их в базе данных"""
+    file = request.files['file']
+    img = io.open("uploads/" + file.filename, 'rb')
+    name = filename.rsplit('/', 1)[-1]
+    path = img.name
+    image = Img(name=name, path=path)
+    try:
+        db.session.add(image)
+        db.session.commit()
+        # print(image)
+        return "ok"
+    except Exception:
+        # print("error adding to database")
+        return "error adding to database"
 
 
 if __name__ == '__main__':
---
+++ readme.txt
@@ -0,0 +1,9 @@
+1. Из директории проекта вызовите консоль
+2 пропишите команды:
+	pyton
+	from example import db
+	db.create_all()
+	exit()
+	python example.py
+3. В Вашем браузере перейдите по адресу http://127.0.0.1:5000/             
+	     	
